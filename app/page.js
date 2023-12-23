7565835cf138db508f8ffe35c7458c0e80484b92
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-12-23 10:23:43 +0545

    router

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>hamro teacher</div>;
+  return (
+    <div>
+      hamro teacher
+      <Link href={"/register"}>Register Page</Link>
+    </div>
+  );
 }
