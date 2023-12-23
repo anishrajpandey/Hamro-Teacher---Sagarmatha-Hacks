230dd005efbd8e0ec93c9f37a2d6018e7b9215b8
PixelPlatypus <manishpoudel2060@gmail.com>
2023-12-23 22:34:25 +0545

    Our Team

--- app/404/page.js
+++ app/404/page.js
@@ -2,18 +2,24 @@ import React from 'react'
 
 function page() {
   return (
-    <div class="flex flex-col justify-center items-center px-6 mx-auto h-screen xl:px-0 dark:bg-gray-900">
-        <div class="block md:max-w-lg">
-            <img src="/images/illustrations/404.svg" alt="astronaut image"/>
-        </div>
-        <div class="text-center xl:max-w-4xl">
-            <h1 class="mb-3 text-2xl font-bold leading-tight text-gray-900 sm:text-4xl lg:text-5xl dark:text-white">Page not found</h1>
-            <p class="mb-5 text-base font-normal text-gray-500 md:text-lg dark:text-gray-400">Oops! Looks like you followed a bad link. If you think this is a problem with us, please tell us.</p>
-            <a href="{}" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-3 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                <svg class="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                Go back home
-            </a>
-        </div>
+    <div>
+      <main className="relative isolate min-h-full w-full">
+      <img
+      src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
+      alt=""
+      className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
+      />
+      <div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
+      <p className="text-base font-semibold leading-8 text-white">404</p>
+      <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">Page not found</h1>
+      <p className="mt-4 text-base text-white/70 sm:mt-6">Sorry, we couldn’t find the page you’re looking for.</p>
+      <div className="mt-10 flex justify-center">
+      <a href="#" className="text-sm font-semibold leading-7 text-white">
+      <span aria-hidden="true">&larr;</span> Back to home
+      </a>
+      </div>
+      </div>
+      </main>
     </div>
   )
 }
--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -5,7 +5,7 @@ function Sidebar() {
     <div>
       <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800">
   <div className="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
-    <div className="flex items-center justify-center h-14 border-b">
+    <div className="flex items-center justify-center h-14 border-b h-25">
       <div><span className='text-[#F79256] font-extrabold'>Hamro</span> Teacher</div>
     </div>
     <div className="overflow-y-auto overflow-x-hidden flex-grow">
