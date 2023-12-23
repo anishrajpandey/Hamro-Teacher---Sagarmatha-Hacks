e2c0a6d433afba7513cdf8d2f29e13a96bb858ba
PixelPlatypus <manishpoudel2060@gmail.com>
2023-12-23 18:07:57 +0545

    added login foewader link in navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -52,7 +52,7 @@ function NavList() {
           Dashboard
         </a>
       </Typography>
-      <a href="#" class="inline-flex items-center justify-center px-4 py-2 mr-3 text-base font-small text-center text-white rounded-lg bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+      <a href="/login" class="inline-flex items-center justify-center px-4 py-2 mr-3 text-base font-small text-center text-white rounded-lg bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Login
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
       </a>
