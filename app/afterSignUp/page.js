cc68a4f10c88922aac7b6125a74a11abd769afdb
Aayush Poudel <aayushpoudel59@gmail.com>
2023-12-23 18:13:05 +0545

    Modified signup, signIn and more

---
+++ app/afterSignUp/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center h-screen font-bold'>
+      <h1>Your response has been recorded. Please wait for a few moments; we'll get back to you soon.</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ app/teacher/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TeacherInfo from '../components/TeacherInfo'
+
+const page = () => {
+  return (
+    <div>
+      <TeacherInfo/>
+    </div>
+  )
+}
+
+export default page
