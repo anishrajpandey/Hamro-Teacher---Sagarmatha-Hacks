b2018a5c958ea6fa4b397f361a66f684bda79788
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-12-23 12:24:17 +0545

    data structured

--- app/_server/RegisterSchool.js
+++ app/_server/RegisterSchool.js
@@ -3,8 +3,31 @@ import { collection, addDoc } from "firebase/firestore";
 import { db } from "./../../firebase/firebase";
 const RegisterSchool = async () => {
   await addDoc(collection(db, "schools"), {
-    name: "ansih",
-    email: "this is m skjdf ",
+    name: "School name",
+    address: "School address ",
+    email: "dfsdf@gmail.com",
+    phone: "difjdfdsf",
+    teachers: [
+      {
+        name: "Teacher's name",
+        contact: "98322118",
+        class: ["1A", "6B", "10C"],
+        Subject: "science",
+        ranking: {
+          quality1: 2,
+          quality2: 2,
+          quality2: 2,
+          quality2: 2,
+          quality2: 2,
+        },
+      },
+    ],
+    students: [
+      {
+        Name: "student1",
+        class: "10c",
+      },
+    ],
   });
 };
 
