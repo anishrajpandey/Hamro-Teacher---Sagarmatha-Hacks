122725a064d3d0d8437f7a796ad7ca3762f4cb74
Aayush Poudel <aayushpoudel59@gmail.com>
2023-12-23 23:07:01 +0545

    hero

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Sidebar() {
@@ -6,7 +7,7 @@ function Sidebar() {
       <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800">
   <div className="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
     <div className="flex items-center justify-center h-14 border-b">
-      <div><span className='text-[#F79256] font-extrabold'>Hamro</span> Teacher</div>
+      <Link href='/'><span className='text-[#F79256] font-extrabold'>Hamro</span> Teacher</Link>
     </div>
     <div className="overflow-y-auto overflow-x-hidden flex-grow">
       <ul className="flex flex-col py-4 space-y-1">
--- app/components/TeacherInfo.js
+++ app/components/TeacherInfo.js
@@ -38,7 +38,7 @@ const TeacherInfo = () => {
               type="text"
               id="name"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              placeholder="First Name"
+              placeholder="Full Name"
               required
             />
           </div>
