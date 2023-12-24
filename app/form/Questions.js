793eab8f743e7ecb68cdb8a43a5b6527b13fc12f
PixelPlatypus <manishpoudel2060@gmail.com>
2023-12-24 07:31:16 +0545

    question.js

--- app/form/Questions.js
+++ app/form/Questions.js
@@ -7,130 +7,73 @@ function Questions() {
     <h2 className="text-2xl font-medium mb-4"> Teachers Name Survey</h2>
     <form>
         <div className="mb-4">
-            <label className="block text-gray-700 font-medium mb-2">Subject Knowledge</label>
+            <label className="block text-gray-700 font-medium mb-2">Subject Knowledge:</label>
             <div className="flex flex-wrap -mx-2">
                 <div className="px-2 w-1/3">
                     <label htmlFor="color-red" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-red" name="color" value="red" className="mr-2"/>Bad
+                        <input type="radio" id="bad" name="subject knowledge" value="1" className="mr-2"/>Bad
                     </label>
                 </div>
                 <div className="px-2 w-1/3">
                     <label htmlFor="color-blue" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-blue" name="color" value="blue" className="mr-2"/>Neutral
+                        <input type="radio" id="neutral" name="subject knowledge" value="2" className="mr-2"/>Neutral
                     </label>
                 </div>
                 <div className="px-2 w-1/3">
                     <label htmlFor="color-green" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-green" name="color" value="green" className="mr-2"/>Great
+                        <input type="radio" id="great" name="subject knowledge" value="3" className="mr-2"/>Great
                     </label>
                 </div>
             </div>
         </div>
 
         <div className="mb-4">
-            <label className="block text-gray-700 font-medium mb-2">Organizational Skills</label>
+            <label className="block text-gray-700 font-medium mb-2">Subject Knowledge:</label>
             <div className="flex flex-wrap -mx-2">
                 <div className="px-2 w-1/3">
                     <label htmlFor="color-red" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-red" name="color" value="red" className="mr-2"/>Bad
+                        <input type="radio" id="bad" name="subject knowledge" value="1" className="mr-2"/>Bad
                     </label>
                 </div>
                 <div className="px-2 w-1/3">
                     <label htmlFor="color-blue" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-blue" name="color" value="blue" className="mr-2"/>Neutral
+                        <input type="radio" id="neutral" name="subject knowledge" value="2" className="mr-2"/>Neutral
                     </label>
                 </div>
                 <div className="px-2 w-1/3">
                     <label htmlFor="color-green" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-green" name="color" value="green" className="mr-2"/>Great
+                        <input type="radio" id="great" name="subject knowledge" value="3" className="mr-2"/>Great
                     </label>
                 </div>
             </div>
         </div>
 
+
         <div className="mb-4">
-            <label className="block text-gray-700 font-medium mb-2">Effective Communication</label>
+            <label className="block text-gray-700 font-medium mb-2">Subject Knowledge:</label>
             <div className="flex flex-wrap -mx-2">
-                <div className="px-2 w-1/3">
+                <div className="px-2 w-fit">
                     <label htmlFor="color-red" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-red" name="color" value="red" className="mr-2"/>Bad
+                        <input type="radio" id="bad" name="subject knowledge" value="1" className="mr-2"/>Bad
                     </label>
                 </div>
-                <div className="px-2 w-1/3">
+                <div className="px-2 w-fit">
                     <label htmlFor="color-blue" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-blue" name="color" value="blue" className="mr-2"/>Neutral
+                        <input type="radio" id="neutral" name="subject knowledge" value="2" className="mr-2"/>Neutral
                     </label>
                 </div>
-                <div className="px-2 w-1/3">
+                <div className="px-2 w-fit">
                     <label htmlFor="color-green" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-green" name="color" value="green" className="mr-2"/>Great
+                        <input type="radio" id="great" name="subject knowledge" value="3" className="mr-2"/>Great
                     </label>
                 </div>
             </div>
         </div>
 
-        <div className="mb-4">
-            <label className="block text-gray-700 font-medium mb-2">Creativity</label>
-            <div className="flex flex-wrap -mx-2">
-                <div className="px-2 w-1/3">
-                    <label htmlFor="color-red" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-red" name="color" value="red" className="mr-2"/>Bad
-                    </label>
-                </div>
-                <div className="px-2 w-1/3">
-                    <label htmlFor="color-blue" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-blue" name="color" value="blue" className="mr-2"/>Neutral
-                    </label>
-                </div>
-                <div className="px-2 w-1/3">
-                    <label htmlFor="color-green" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-green" name="color" value="green" className="mr-2"/>Great
-                    </label>
-                </div>
-            </div>
-        </div>
 
-        <div className="mb-4">
-            <label className="block text-gray-700 font-medium mb-2">Student Friendly</label>
-            <div className="flex flex-wrap -mx-2">
-                <div className="px-2 w-1/3">
-                    <label htmlFor="color-red" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-red" name="color" value="red" className="mr-2"/>Bad
-                    </label>
-                </div>
-                <div className="px-2 w-1/3">
-                    <label htmlFor="color-blue" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-blue" name="color" value="blue" className="mr-2"/>Neutral
-                    </label>
-                </div>
-                <div className="px-2 w-1/3">
-                    <label htmlFor="color-green" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-green" name="color" value="green" className="mr-2"/>Great
-                    </label>
-                </div>
-            </div>
-        </div>
 
-        <div className="mb-4">
-            <label className="block text-gray-700 font-medium mb-2">Classroom</label>
-            <div className="flex flex-wrap -mx-2">
-                <div className="px-2 w-1/3">
-                    <label htmlFor="color-red" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-red" name="color" value="red" className="mr-2"/>Bad
-                    </label>
-                </div>
-                <div className="px-2 w-1/3">
-                    <label htmlFor="color-blue" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-blue" name="color" value="blue" className="mr-2"/>Neutral
-                    </label>
-                </div>
-                <div className="px-2 w-1/3">
-                    <label htmlFor="color-green" className="block text-gray-700 font-medium mb-2">
-                        <input type="radio" id="color-green" name="color" value="green" className="mr-2"/>Great
-                    </label>
-                </div>
-            </div>
-        </div>
+
+
         <div className="mb-4">
             <label htmlFor="message" className="block text-gray-700 font-medium mb-2">Message</label>
             <textarea id="message" name="message"
